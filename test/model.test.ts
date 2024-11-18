265f286659ce6e8086bf68d73c0ce2863ee23762
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-11-18 11:31:57 +0900

    Initial commit

--- test/model.test.ts
+++ test/model.test.ts
@@ -1,9 +1,23 @@
 import {describe, expect, test} from '@jest/globals';
-import {Card, makeCard } from '../src/model/model';
+import {Card, getGuesses, Guesses, makeCard } from '../src/model/model';
 
 test("Return a card", ()=>{
     const expectedCard:Card = {frontSide:"library", backSide:"cat"}
     const results = makeCard("library", "cat")
 
     expect(results).toEqual(expectedCard)
+})
+
+test("Return a guesses object", ()=>{
+    const expected:Guesses = {
+        firstGuess: false,
+        secondGuess: false,
+        thirdGuess: false,
+        fourthGuess: false,
+        fifthGuess: false,
+      };
+
+    const results = getGuesses()
+
+    expect(results).toEqual(expected)
 })
